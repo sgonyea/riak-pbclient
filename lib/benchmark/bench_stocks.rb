c76a1bc2670d7f2d5af9381a73b3f79b0375a2a0
Scott Gonyea <me@sgonyea.com>
2010-12-30 14:38:35 -0800

    normal load script + benchmarking version

---
+++ lib/benchmark/bench_stocks.rb
@@ -0,0 +1,26 @@
+require 'rubygems' if RUBY_VERSION < '1.9'
+require 'bundler'
+
+Bundler.require
+
+require 'csv'
+require 'benchmark'
+
+@noisy = ARGV.grep("-q").none?
+
+Benchmark.bmbm do |x|
+  x.report do
+
+    client = Riakpb::Client.new
+    bucket = client["goog"]
+
+    CSV.foreach('goog.csv', :headers => true) do |row|
+      @noisy && puts(row.first[1].to_s)
+
+      key = bucket[row.first[1].to_s]
+      key.content.value = Hash[row.to_a]
+      key.save
+    end
+
+  end
+end
--- load_stocks.rb
+++ load_stocks.rb
@@ -8,19 +8,13 @@ require 'benchmark'
 
 @noisy = ARGV.grep("-q").none?
 
-Benchmark.bmbm do |x|
-  x.report do
+client = Riakpb::Client.new
+bucket = client["goog"]
 
-    client = Riakpb::Client.new
-    bucket = client["goog"]
+CSV.foreach('goog.csv', :headers => true) do |row|
+  @noisy && puts(row.first[1].to_s)
 
-    CSV.foreach('goog.csv', :headers => true) do |row|
-      @noisy && puts row.first[1].to_s
-
-      key = bucket[row.first[1].to_s]
-      key.content.value = Hash[row.to_a]
-      key.save
-    end
-
-  end
+  key = bucket[row.first[1].to_s]
+  key.content.value = Hash[row.to_a]
+  key.save
 end
